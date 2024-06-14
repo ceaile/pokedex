bb581dd421e6a2a901622d7c5c9baaaf0408e20b
ceaile <ceaile1994@protonmail.com>
2024-06-14 16:13:28 +0200

    arreglada pag de equipos

--- public/vistas/misequipos.php
+++ public/vistas/misequipos.php
@@ -44,52 +44,208 @@ array (
 
 */
 ?>
-<div class="pokemon-container" style="opacity: 0;">
+<div class="pokemon-container pokemon-container-equipos" style="opacity: 0;">
   <!-- Iteración de equipos -->
   <?php foreach ($equipos as $equipo): ?>
-    <div class="equipo" style="margin-bottom: 15px; overflow-x: auto; white-space: nowrap;">
+    <div class="equipo div-equipo">
       <!-- Estilo añadido para desplazamiento horizontal -->
-      <div style="text-align: center;"> <!-- Contenedor centrado para el título y el botón -->
-        <div
-          style="background-color: white; border-radius: 5px; display: inline-block; padding: 10px; margin-bottom: 10px;">
+      <div class="div-intern-equipo"> <!-- Contenedor centrado para el título y el botón -->
+        <div class="div-intern2-equipo">
           <!-- Estilo para el contenedor del título y botón -->
-          <h3
-            style="margin: 0; background-color:white; display: inline-flex; align-items: center; justify-content: center;">
+          <h3 class="nombre-equipo">
             <?= htmlspecialchars($equipo['nombre']) ?>
           </h3> <!-- Título -->
-          <a href="#renameModal" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
-            data-id="<?= htmlspecialchars($equipo['id']) ?>" style="margin-left: 10px;">Rename</a> <!-- Botón -->
+          <a href="#renameModal" class="link-renombre" data-id="<?= htmlspecialchars($equipo['id']) ?>">Rename</a>
+          <!-- Botón -->
         </div>
       </div>
 
-      <div style="text-align: center; display: flex; justify-content: center; align-items: center; overflow-x: auto;">
+      <div class="div-seis-pkmns">
+
         <!-- Contenedor centrado y flexible para los Pokémon -->
         <?php foreach ($equipo['seisPokemons'] as $pokemon): ?>
           <?php if ($pokemon['id_pokemon'] != 0): ?>
             <div class="pokemon-img-container"
               onclick="confirmarEliminacion(<?= htmlspecialchars($pokemon['id_equipopokemon']) ?>)"
-              style="display: inline-block; margin-right: 10px; padding: 2px; background-color: white; border-radius: 5px; position: relative;">
+              class="boton-menos-quitar-pkmn">
               <img src="<?= htmlspecialchars($pokemon['art']) ?>" id="<?= htmlspecialchars($pokemon['id_equipopokemon']) ?>"
-                name="<?= htmlspecialchars($pokemon['id_equipopokemon']) ?>" class="w-48 h-48" style="display: block;">
+                name="<?= htmlspecialchars($pokemon['id_equipopokemon']) ?>" class="w-48 h-48 sprite-pkmn">
             </div>
           <?php endif; ?>
         <?php endforeach; ?>
       </div>
-      <div style="text-align: center; display: flex; justify-content: center; align-items: center; overflow-x: auto;">
+
+      <div class="div-seis-pkmns">
         <!-- Contenedor centrado y flexible para los botones "View" -->
         <?php foreach ($equipo['seisPokemons'] as $pokemon): ?>
           <?php if ($pokemon['id_pokemon'] != 0): ?>
             <a href="card?id_pokemon=<?= htmlspecialchars($pokemon['id_pokemon']) ?>"
-              class="view-button-mobile bg-yellow-500 hover:bg-yellow-700 text-white px-2 py-1 rounded-lg mt-2 block text-center"
-              style="width: 12rem; margin-right: 10px;">View</a>
+              class="view-button-mobile bg-yellow-500 hover:bg-yellow-700 text-white px-2 py-1 
+              block text-center boton-ver-pkmn">View</a>
           <?php endif; ?>
         <?php endforeach; ?>
       </div>
     </div>
   <?php endforeach; ?>
 </div>
-<br><br>
 
+
+
+<style>
+/*
+  .pokemon-container-equipos {
+    background-color: var(--blanco);
+    margin: 0 auto;
+    border-radius: 20px;
+    margin-top: 5vh;
+    margin-bottom: 5vh;
+    height: 70vh;
+    overflow-y: auto;
+    width: 70%;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+
+  @media (max-width: 1240px) {
+    .pokemon-container-equipos {
+        margin: 0 auto;
+        width: 90%;
+    }
+    .div-intern-equipo {
+      margin-left:0px;
+      text-align: center;}
+  }
+  
+  .div-equipo {
+    margin-bottom: 15px;
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+
+  .div-intern-equipo {
+    text-align: center;
+  }
+
+  .div-intern2-equipo {
+    border-radius: 15px;
+    display: inline-block;
+    padding: 10px;
+    margin-top: 10px;
+  }
+
+  .nombre-equipo {
+    margin: 0;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+    font-weight: bolder;
+  }
+
+  .link-renombre {
+    margin-left: 10px;
+    font-size: smaller;
+    text-decoration-line: underline;
+    padding: 5px;
+  }
+
+  .link-renombre:hover {
+    background-color: aqua;
+    border-radius: 15px;
+  }
+
+  .sprite-pkmn {
+    display: block;
+    border-radius: inherit;
+    border-color: black;
+    border-width: 1px;
+    width: 400px;
+    height: auto;
+  }
+
+  .div-seis-pkmns {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow-x: auto;
+  }
+
+  .boton-menos-quitar-pkmn {
+    display: inline-block;
+    margin-right: 10px;
+    padding: 2px;
+    background-color: white;
+    border-radius: 5px;
+    position: relative;
+  }
+
+  .boton-add-pkmn {
+    width: 12rem;
+    margin-right: 10px;
+  }
+
+  .boton-ver-pkmn {
+    width: 14em;
+    margin-right: 0px;
+  }
+
+  .pokemon-img-container {
+    background-color: white;
+    border-radius: 25px;
+    padding: 0px;
+    display: inline-block;
+    position: relative;
+    border-color: var(--negro);
+    border-style: solid;
+  }
+
+  .pokemon-img-container:hover::after {
+    content: '-';
+    font-size: 50px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    padding-left:30px;
+    padding-right:30px;
+    transform: translate(-50%, -50%);
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 5px 20px;
+    border-radius: 10px;
+    white-space: nowrap;
+    z-index: 10;
+  }
+
+  @media (max-width: 600px) {
+    .pokemon-img-container img {
+      width: 100px;
+      height: auto;
+    }
+
+    .pokemon-img-container:hover::after {
+      font-size: 10px;
+    }
+
+    .view-button-mobile {
+      display: none;
+    }
+  } */
+</style>
+
+
+
+
+
+
+
+
+<!-- MODALES: -->
 <!-- Modal de eliminar Pokémon -->
 <div id="modal-conf-eliminacion"
   class="hidden flex items-center justify-center fixed inset-0 z-50 overflow-auto bg-gray-500 bg-opacity-75">
@@ -157,51 +313,5 @@ array (
     </div>
   </div>
 </div>
-
-
-<style>
-  .pokemon-img-container {
-    background-color: white;
-    border-radius: 3px;
-    /* Reducido el radio del borde */
-    padding: 1px;
-    /* Reducido el espacio entre la imagen y el borde blanco */
-    display: inline-block;
-    position: relative;
-    /* Necesario para el pseudo-elemento */
-  }
-
-  .pokemon-img-container:hover::after {
-    content: '-';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background: rgba(0, 0, 0, 0.5);
-    color: white;
-    padding: 5px 10px;
-    border-radius: 5px;
-    white-space: nowrap;
-    z-index: 10;
-  }
-
-  @media (max-width: 600px) {
-    .pokemon-img-container img {
-      width: 100px;
-      /* Ajusta este valor según tus necesidades */
-      height: auto;
-    }
-
-    .pokemon-img-container:hover::after {
-      font-size: 10px;
-      /* Ajusta este valor según tus necesidades */
-    }
-
-    .view-button-mobile {
-      display: none;
-      /* Oculta el botón "View" */
-    }
-  }
-</style>
 
 <script src="../js/editar-equipos.js" type="text/javascript"></script>
